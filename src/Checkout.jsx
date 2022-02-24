9191f07f5485ceb118d9ab070819fbd57aa8d1b0
Josvany García <josvanyg@gmail.com>
2022-02-24 13:02:58 -0600

    validacion en formulario

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { saveShippingAddress } from "./services/shippingService";
 
 const STATUS = {
-  IDLE : "IDLE",
-  SUBMITTED : "SUBMITTED",
+  IDLE: "IDLE",
+  SUBMITTED: "SUBMITTED",
   SUBMITTING: "SUBMITTING",
-  COMPLETED : "COMPLETED"
+  COMPLETED: "COMPLETED",
 };
 
 // Declaring outside component to avoid recreation on each render
@@ -15,52 +16,73 @@ const emptyAddress = {
 };
 
 export default function Checkout({ cart, emptyCart }) {
-  const [status, setStatus] = useState(STATUS.IDLE);
   const [address, setAddress] = useState(emptyAddress);
+  const [status, setStatus] = useState(STATUS.IDLE);
   const [saveError, setSaveError] = useState(null);
+  const [touched, setTouched] = useState({});
+
+  // Derived state
+  const errors = getErrors(address);
+  const isValid = Object.keys(errors).length === 0;
 
   function handleChange(e) {
-    // TODO
-    // e.parsist(); // si la version es menor a 17 se debe persitir por que de lo contrario da error
-    setAddress((curAddress) => 
-    {
+    e.persist(); // persist the event
+    setAddress((curAddress) => {
       return {
         ...curAddress,
-        [e.target.id] : e.target.value,
-      }
+        [e.target.id]: e.target.value,
+      };
     });
   }
 
   function handleBlur(event) {
-    // TODO
+    event.persist();
+    setTouched((cur) => {
+      return { ...cur, [event.target.id]: true };
+    });
   }
 
-  async function handleSubmit(event) 
-  {
+  async function handleSubmit(event) {
     event.preventDefault();
     setStatus(STATUS.SUBMITTING);
-    try 
-    {
-      await saveShippingAddress(address);
-      emptyCart();
-      setStatus(STATUS.COMPLETED);
+    if (isValid) {
+      try {
+        await saveShippingAddress(address);
+        emptyCart();
+        setStatus(STATUS.COMPLETED);
+      } catch (e) {
+        setSaveError(e);
+      }
+    } else {
+      setStatus(STATUS.SUBMITTED);
     }
-    catch (e) 
-    {
-      setSaveError(e);
-    }
+  }
+
+  function getErrors(address) {
+    const result = {};
+    if (!address.city) result.city = "City is required";
+    if (!address.country) result.country = "Country is required";
+    return result;
   }
 
   if (saveError) throw saveError;
-
-  if (status === STATUS.COMPLETED)
-  {
-    return <h1>Thanks for shopping</h1>
+  if (status === STATUS.COMPLETED) {
+    return <h1>Thanks for shopping!</h1>;
   }
 
   return (
     <>
       <h1>Shipping Info</h1>
+      {!isValid && status === STATUS.SUBMITTED && (
+        <div role="alert">
+          <p>Please fix the following errors:</p>
+          <ul>
+            {Object.keys(errors).map((key) => {
+              return <li key={key}>{errors[key]}</li>;
+            })}
+          </ul>
+        </div>
+      )}
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="city">City</label>
@@ -72,6 +94,9 @@ export default function Checkout({ cart, emptyCart }) {
             onBlur={handleBlur}
             onChange={handleChange}
           />
+          <p role="alert">
+            {(touched.city || status === STATUS.SUBMITTED) && errors.city}
+          </p>
         </div>
 
         <div>
@@ -89,10 +114,19 @@ export default function Checkout({ cart, emptyCart }) {
             <option value="United Kingdom">United Kingdom</option>
             <option value="USA">USA</option>
           </select>
+
+          <p role="alert">
+            {(touched.country || status === STATUS.SUBMITTED) && errors.country}
+          </p>
         </div>
 
         <div>
-          <input type="submit" className="btn btn-primary" value="Save Shipping Info" disabled={status === STATUS.SUBMITTING} />
+          <input
+            type="submit"
+            className="btn btn-primary"
+            value="Save Shipping Info"
+            disabled={status === STATUS.SUBMITTING}
+          />
         </div>
       </form>
     </>
